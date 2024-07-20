2aaa005b68d58fb07f403d4e68cd34be343248f4
farissbayu <bayustfa@gmail.com>
2024-07-20 23:29:04 +0700

    integrate catatan page

--- src/app/(main-feature)/catatan/page.js
+++ src/app/(main-feature)/catatan/page.js
@@ -1,26 +1,100 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import TipTap from "@/components/tiptap";
+import Cookies from "universal-cookie";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
 
 export default function CatatanPage() {
-  const [content, setContent] = useState("");
-  const handleContentChange = (reason) => setContent(reason);
+  const cookie = new Cookies();
+  const token = cookie.get("token");
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const [submissionLoading, setSubmissionLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState(null);
+
+  const handleContentChange = (newContent) => {
+    setContent(newContent);
+  };
+
+  async function getCatatan() {
+    setLoading(true);
+    try {
+      const res = await axios.get("/api/catatan", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      setContent(res.data.data.content);
+    } catch (error) {
+      console.error("Error fetching catatan:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getCatatan();
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setSubmissionLoading(true);
+    const requestBody = { content: content };
+
+    try {
+      const response = await axios.post("/api/catatan", requestBody, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      console.log("Data saved:", response.data);
+    } catch (error) {
+      console.error("Error saving catatan:", error);
+    } finally {
+      toast({
+        description: "Berhasil menyimpan catatan",
+      });
+      setSubmissionLoading(false);
+    }
+  }
+
+  function close(e) {
+    e.preventDefault();
+    router.back();
+  }
 
   return (
     <div className="h-screen flex flex-col items-center p-8">
       <div className="w-full min-h-[300px] max-h-[480px]">
-        <form className="flex flex-col space-y-4 w-full h-full">
+        <form
+          className="flex flex-col space-y-4 w-full h-full my-auto"
+          onSubmit={handleSubmit}
+        >
           <h1 className="font-bold text-xl">Catatan</h1>
-          <TipTap
-            content={content}
-            onChange={(newContent) => handleContentChange(newContent)}
-          />
+          {loading && <p>Loading...</p>}
+          {content !== null && (
+            <TipTap content={content} onChange={handleContentChange} />
+          )}
           <div className="flex flex-row space-x-4 justify-center items-center w-full">
-            <Button className="bg-emerald-700 rounded-xl text-white hover:bg-emerald-800">
-              Simpan
+            <Button
+              variant="outline"
+              className="border border-2 border-black rounded-xl text-black hover:bg-emerald-700/5"
+              onClick={close}
+            >
+              Tutup
+            </Button>
+            <Button
+              className="bg-emerald-700 rounded-xl text-white hover:bg-emerald-800"
+              type="submit"
+            >
+              {submissionLoading ? "Loading..." : "Simpan"}
             </Button>
           </div>
         </form>
